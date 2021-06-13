4692ca6718d89bdec87cb7bfdc57857d6ab52fab
Rupesh <rupeshkumar9312@gmail.com>
2021-06-13 16:06:42 +0530

    initial-commit

---
+++ src/test/java/com/udemy/photoapp/api/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.photoapp.api.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
